659bafabf9308ba07c9d9016e5de4ba77889ca0b
kuwabara146 <toma10256@gmail.com>
2020-02-22 15:45:28 +0900

    課題２

---
+++ Batch/src/task2.java
@@ -0,0 +1,14 @@
+
+public class task2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        /*
+          コメント
+          コメント
+        */
+		System.out.println("hogehoge");
+		System.out.print("hogehoge2");
+	}
+
+}
