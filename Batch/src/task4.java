8c00f13a0355357a4051a49a927a85ccc253bd0a
kuwabara146 <toma10256@gmail.com>
2020-02-25 20:46:25 +0900

    task4

---
+++ Batch/src/task4.java
@@ -0,0 +1,15 @@
+
+public class task4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        String name = "山田　太郎";
+
+        System.out.println(name + "です。");
+
+        final String TELPHONE_NUMBER = "090-9999-1111";
+
+        System.out.println("私の電話番号は\'" + TELPHONE_NUMBER + "\'です。");
+	}
+
+}
