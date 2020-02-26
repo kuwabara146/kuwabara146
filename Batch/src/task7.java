e447bb90815ec3156553124f077f908f2ece846c
kuwabara146 <toma10256@gmail.com>
2020-02-26 20:22:55 +0900

    task7

---
+++ Batch/src/task7.java
@@ -0,0 +1,19 @@
+
+public class task7 {
+
+	public static void main(String[] args) {
+		int a = 10, b = 12, c = 13;
+		int d = a + b + c;      //合計
+
+		System.out.println("合計:"+ d);
+
+		int e = d / 3;          //3で割った時の平均
+
+		System.out.println("平均(3で割った時):" + e);
+
+		double f = d / 3.0;    //3.0で割った時の平均
+
+		System.out.println("平均(3.0で割った時):" + f);
+	}
+
+}
