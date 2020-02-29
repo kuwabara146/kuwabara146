cee614b01bc9546fb1d30c834d37e76a68f1547b
kuwabara146 <toma10256@gmail.com>
2020-02-29 16:33:25 +0900

    task23

---
+++ Batch/src/task23.java
@@ -0,0 +1,26 @@
+
+public class task23 {
+
+	public static void main(String[] args) {
+		int[] array = {4,9,19,5,8,21,42,64,73,18,2};
+
+		System.out.print("配列: [");
+
+		for(int i = 0; i < array.length; i++) {
+			System.out.print(array[i]);
+			if(i < array.length - 1) {
+				System.out.print(",");
+			}
+		}
+
+		System.out.println("]");
+
+		int sum = 0;
+		for(int j : array) {
+			sum += j;
+		}
+
+		System.out.println("合計: " + sum);
+	}
+
+}
