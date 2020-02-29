5fe4f183bbc082b8c07ef1dfef1d00adcca00bf1
kuwabara146 <toma10256@gmail.com>
2020-02-29 14:30:35 +0900

    task19

---
+++ Batch/src/task19.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class task19 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("数字を入力してください: ");
+		int num = sc.nextInt();
+		sc.close();
+
+		int sum = 0;
+		int i = 1;
+		while(i < num) {
+			sum += i;
+			System.out.print(i + " + ");
+			i++;
+		}
+
+		sum += i;
+		System.out.println(i + " = " + sum);
+	}
+
+}
