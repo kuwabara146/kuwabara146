485f4031c9380a941fc02af2cbf3c42dcbbc0d8d
kuwabara146 <toma10256@gmail.com>
2020-03-02 20:12:30 +0900

    課題提出 - 配列

---
+++ array/src/task2.java
@@ -0,0 +1,30 @@
+
+public class task2 {
+
+	public static void main(String[] args) {
+		int[] array = new int[7];
+		int a = 7;
+
+		for(int i = 0; i < array.length; i++) {
+			array[i] = a;
+			a--;
+		}
+
+		for(int j = 0; j < array.length; j++) {
+			System.out.print(array[j]);
+			if(j < array.length - 1) {
+				System.out.print(",");
+			}
+		}
+
+		System.out.println();
+
+		for(int k = array.length - 1; k >= 0; k--) {
+			System.out.print(array[k]);
+			if(k > 0) {
+				System.out.print(",");
+			}
+		}
+	}
+
+}
---
+++ array/src/task3.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+public class task3 {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		int[] intArray = new int[6];
+
+		System.out.print("[");
+		for(int i = 0; i < intArray.length; i++) {
+			intArray[i] = random.nextInt(10);
+			System.out.print(intArray[i]);
+			if(i < intArray.length - 1) {
+				System.out.print(",");
+			}
+		}
+		System.out.println("]");
+
+		int max = intArray[0];
+		int min = intArray[0];
+		for(int j = 0; j < intArray.length; j++) {
+			if(max < intArray[j]) {
+				max = intArray[j];
+			}
+			if(min > intArray[j]) {
+				min = intArray[j];
+			}
+		}
+		System.out.println("最大値: " + max);
+		System.out.println("最小値: " + min);
+	}
+
+}
