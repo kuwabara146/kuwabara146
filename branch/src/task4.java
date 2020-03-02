70d57d458a5ac5039477e6fc0517b4650c76e358
kuwabara146 <toma10256@gmail.com>
2020-03-02 20:05:31 +0900

    課題提出 - 条件分岐

---
+++ branch/src/task4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class task4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int numA = sc.nextInt();
+		System.out.println("numA: " + numA);
+		int numB = sc.nextInt();
+		System.out.println("numB: " + numB);
+		sc.close();
+
+		if(numA < 1 || numB < 1) {
+			System.out.println("正の整数を入力してください");
+		}else if(numA % numB == 0) {
+			System.out.println("numBはnumAの約数です");
+		}else {
+			System.out.println("numBはnumAの約数ではありません");
+		}
+
+	}
+
+}
