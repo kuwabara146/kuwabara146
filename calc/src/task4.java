4b3a10ff6adbc92277a0992fa92c226f7aef949b
kuwabara146 <toma10256@gmail.com>
2020-03-02 20:02:43 +0900

    課題提出 - 四則演算

---
+++ calc/src/task4.java
@@ -0,0 +1,14 @@
+
+public class task4 {
+
+	public static void main(String[] args) {
+		int a = 192837;
+		int b = a / 10;
+		int c = a % (b * 10);
+
+		System.out.println("一の位を除く数:" + b);
+
+		System.out.println("一の位の数:" + c);
+	}
+
+}
