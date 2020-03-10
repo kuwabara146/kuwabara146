c1c618fbae96edba7608a0b1b58a83794093d720
kuwabara146 <toma10256@gmail.com>
2020-03-10 23:28:35 +0900

    課題提出 - インスタンス＆クラス

---
+++ instance/src/Task2.java
@@ -0,0 +1,12 @@
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        Person kuwabara = new Person();
+
+        kuwabara.name = "桑原 将人";
+        kuwabara.age = 26;
+
+    }
+
+}
---
+++ instance/src/Task3.java
@@ -0,0 +1,16 @@
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        //インスタンスの生成
+        Person kuwabara = new Person();
+
+        //フィールドに値を設定
+        kuwabara.name = "桑原 将人";
+        kuwabara.age = 26;
+
+        //インスタンスメソッドの実行
+        kuwabara.myProfile();
+    }
+
+}
